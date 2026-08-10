f0dac0f905a372f7bb74bbc33e7182c105f4c969
soraluasom-commits <sora.luasom@gmail.com>
2026-08-11 06:33:46 +0700

    Update service worker for V1.10.8 Web

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-const CACHE='barber-manager-v2-0-2';
-const ASSETS=["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./css/01.css", "./css/02.css", "./css/03.css", "./css/04.css", "./css/05.css", "./js/01.js", "./js/02.js", "./js/03.js", "./js/04.js", "./js/05.js", "./js/06.js", "./js/07.js"];
-
-self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));});
-self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))));});
+const CACHE='barber-manager-v1-10-8-web';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(hit=>hit||caches.match('./index.html'))))});
