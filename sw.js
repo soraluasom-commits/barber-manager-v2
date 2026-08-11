98db5c11f1503a05cfdff0c161f8b8ff71601ce7
soraluasom-commits <sora.luasom@gmail.com>
2026-08-11 17:12:31 +0700

    Bump service worker cache to V1.10.8.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='barber-manager-v1-10-8-web';
+const CACHE='barber-manager-v1-10-8-1-web';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
